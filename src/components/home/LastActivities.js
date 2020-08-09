6e9fb11c3b152b60d75fcd4e6ca2cc3cf0138f5e
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2020-08-09 11:28:05 +0100

    kamalt el last activities

--- src/components/home/LastActivities.js
+++ src/components/home/LastActivities.js
@@ -1,17 +1,68 @@
 import React from "react";
+import { Row, Col, Button } from "react-bootstrap";
+import act1 from "./img/activities/act1.png";
+import act2 from "./img/activities/act2.png";
+import act3 from "./img/activities/act3.png";
 
 const LastActivities = () => {
   return (
-    <section>
-      {/* <h1>Last Activities</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias nostrum
-        nobis vel praesentium ducimus. Hic, minima velit odio fugit vel
-        laudantium, voluptas veritatis repudiandae suscipit nesciunt quibusdam
-        nam eveniet veniam corrupti? Veniam veritatis autem consequuntur? Fugiat
-        ut dolorum illo, placeat tempore magni possimus expedita omnis eum
-        distinctio, reiciendis eveniet quo.
-      </p> */}
+    <section className="activities-section">
+      <Row>
+        <h1 className="title">Last Activities</h1>
+      </Row>
+      <Row className="row-activities">
+        {/* Act1 */}
+        <Col className="box-activities" lg={{ span: 3 }} md={7}>
+          <Row>
+            <Col>
+              <img src={act1} alt="act1" />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <h2>Title here</h2>
+              <p>short description</p>
+              <Button className="mb-3" variant="outline-primary" block>
+                More
+              </Button>
+            </Col>
+          </Row>
+        </Col>
+
+        <Col className="box-activities" lg={{ span: 3 }} md={7}>
+          <Row>
+            <Col>
+              <img src={act2} alt="act2" />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <h2>Title here</h2>
+              <p>short description</p>
+              <Button className="mb-3" variant="outline-primary" block>
+                More
+              </Button>
+            </Col>
+          </Row>
+        </Col>
+
+        <Col className="box-activities" lg={{ span: 3 }} md={7}>
+          <Row>
+            <Col>
+              <img src={act3} alt="act3" />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <h2>Title here</h2>
+              <p>short description</p>
+              <Button className="mb-3" variant="outline-primary" block>
+                More
+              </Button>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
     </section>
   );
 };
