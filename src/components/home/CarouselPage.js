e9052d2dadb5b2870a223bf2ba1023649fb2af66
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2020-08-09 13:35:57 +0100

    9e3ed ngued fel carousel

--- src/components/home/CarouselPage.js
+++ src/components/home/CarouselPage.js
@@ -2,77 +2,76 @@ import React from "react";
 import carr5 from "./img/carousel/carr5.jpg";
 import carr2 from "./img/carousel/carr2.jpg";
 import carr3 from "./img/carousel/carr1.jpg";
+
 import { Carousel } from "react-bootstrap";
-import Typical from "react-typical";
+// import Typical from "react-typical";
+import { Link } from "react-router-dom";
 
 const CarouselPage = () => {
   return (
-    <Carousel interval={7500} style={{ position: "relative", zIndex: "0" }}>
-      <Carousel.Item>
-        <img
-          style={{ maxHeight: "74vh" }}
-          className="d-block w-100"
-          src={carr5}
-          alt="First slide"
-        />
+    <Carousel
+      indicators={false}
+      interval={3000}
+      style={{ position: "relative", zIndex: "0" }}
+    >
+      <Carousel.Item className="item">
+        <div
+          className="banner"
+          style={{ backgroundImage: `url(${carr5})` }}
+        ></div>
         <Carousel.Caption>
           <div className="carousel-caption">
-            <h1>IEEE INSAT EMBS Chapter</h1>
+            {/* Check this for animation https://animate.style/ */}
+            <h2
+              className="animate__animated animate__fadeInUp"
+              style={{ animationDelay: "1s" }}
+            >
+              IEEE INSAT <span>EMBS</span> Chapter
+            </h2>
             <h3>
-              Your Global Connection
-              <Typical
-                loop={Infinity}
-                wrapper="b"
-                steps={["  To The World Of Biomedical Engineering ", 3500]}
-              />
+              Your Global Connection To The World Of Biomedical Engineering
             </h3>
+            <p>
+              <Link to="#">Contact us</Link>
+            </p>
           </div>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
-        <img
-          style={{ maxHeight: "74vh" }}
-          className="d-block w-100"
-          src={carr3}
-          alt="First slide"
-        />
+      <Carousel.Item className="item">
+        <div
+          className="banner"
+          style={{ backgroundImage: `url(${carr2})` }}
+        ></div>
         <Carousel.Caption>
           <div className="carousel-caption">
-            <h3>We work to enhance</h3>
-            <h1>Students Skills</h1>
+            <h2>
+              IEEE INSAT <span>EMBS</span> Chapter
+            </h2>
             <h3>
-              Through
-              <Typical
-                loop={Infinity}
-                wrapper="b"
-                steps={[
-                  " Workshops",
-                  900,
-                  " Events",
-                  900,
-                  " Conferences",
-                  900,
-                  " Magasines",
-                  900,
-                ]}
-              />
+              Your Global Connection To The World Of Biomedical Engineering
             </h3>
+            <p>
+              <Link to="#">Contact us</Link>
+            </p>
           </div>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
-        <img
-          style={{ maxHeight: "74vh" }}
-          className="d-block w-100"
-          src={carr2}
-          alt="First slide"
-        />
+      <Carousel.Item className="item">
+        <div
+          className="banner"
+          style={{ backgroundImage: `url(${carr3})` }}
+        ></div>
         <Carousel.Caption>
           <div className="carousel-caption">
-            <h1>
-              Ready
-              <br /> To Be part of our journey ?
-            </h1>
+            <h2>
+              IEEE INSAT <span>EMBS</span> Chapter
+            </h2>
+            <h3>
+              Your Global Connection To The World Of Biomedical Engineering
+            </h3>
+            <p>
+              <Link to="#">Contact us</Link>
+            </p>
           </div>
         </Carousel.Caption>
       </Carousel.Item>
