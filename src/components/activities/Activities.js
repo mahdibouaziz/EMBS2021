c97bdb3e389fba50317984c9830f2e1df5590440
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2020-08-13 10:55:04 +0100

    bdit ngued fel activities page

--- src/components/activities/Activities.js
+++ src/components/activities/Activities.js
@@ -1,12 +1,13 @@
 import React from "react";
 import pic from "./img/events/biiot_challenge.jpg";
 import "./styleAct.css";
-import { Container, Row, Col } from "react-bootstrap";
 
 const Activities = () => {
   return (
     <div>
-      <h2>act</h2>
+      <div className="banner" style={{ backgroundImage: `url(${pic})` }}>
+        <h1 className="banner-text">Activities</h1>
+      </div>
     </div>
   );
 };
