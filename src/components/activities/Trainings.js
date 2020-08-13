d203eff5269d94bdec953faaf677122299f97b56
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2020-08-13 12:52:22 +0100

    kamalt raka7t l zina mte3 l trainings

--- src/components/activities/Trainings.js
+++ src/components/activities/Trainings.js
@@ -7,65 +7,76 @@ import python_training from "./img/trainings/python_training.jpg";
 
 const Trainings = () => {
   return (
-    <Container className="section">
-      {/* check https://github.com/michalsnik/aos#animations for the animation */}
-      <Row>
-        <h1 className="title">
-          <span>Trainings</span> We Provide
-        </h1>
-      </Row>
+    <div style={{ backgroundColor: "#f2f2f2f2" }}>
+      <Container className="section">
+        {/* check https://github.com/michalsnik/aos#animations for the animation */}
+        <Row>
+          <h1 className="title training-header">
+            <span>Trainings</span> We Provide
+          </h1>
+        </Row>
 
-      <Row className="main-row">
-        <Col className="box" md={{ span: 5 }} sm={{ span: 5 }}>
-          <p>
-            <i class="fas fa-cogs"></i>
-          </p>
-          <p className="title-box">Technical Skills Trainings</p>
-          <p className="date">
-            <i class="far fa-calendar-check"></i> February 2018
-          </p>
-          <p className="desc">
-            In collaboration with the IMDTEC Tunisie, IEEE EMBS INSAT SB has put
-            at the disposal of some of its members a certified ISO 17025
-            training.
-          </p>
-          <img src={iso_training} alt="pic" />
-          <p className="date">
-            <i class="far fa-calendar-check"></i> November, 2018
-          </p>
-          <p className="desc">
-            IEEE EMBS chapter INSAT gave an introduction to Bio-informatics
-            through a workshop combining biology, computer science and
-            mathematics conducted by Mr Ali Chamkhi
-          </p>
-          <img src={python_training} alt="pic" />
-        </Col>
+        <Row className="main-row">
+          <Col className="box" md={{ span: 5 }} sm={{ span: 12 }}>
+            <p className="training-icon">
+              <i className="fas fa-cogs"></i>
+            </p>
+            <p className="training-title">Technical Skills Trainings</p>
+            <p className="training-date">
+              <i className="far fa-calendar-check"></i> February 2018
+            </p>
+            <p className="training-desc">
+              In collaboration with the IMDTEC Tunisie, IEEE EMBS INSAT SB has
+              put at the disposal of some of its members a certified ISO 17025
+              training.
+            </p>
+            <img className="training-img" src={iso_training} alt="pic" />
+            <p className="training-date">
+              <i className="far fa-calendar-check"></i> November, 2018
+            </p>
+            <p className="training-desc">
+              IEEE EMBS chapter INSAT gave an introduction to Bio-informatics
+              through a workshop combining biology, computer science and
+              mathematics conducted by Mr Ali Chamkhi
+            </p>
+            <img className="training-img" src={python_training} alt="pic" />
+          </Col>
 
-        <Col className="box" md={{ span: 5 }} sm={{ span: 5 }}>
-          <p>
-            <i class="fas fa-cogs"></i>
-          </p>
-          <p className="title-box">Soft Skills Trainings</p>
-          <p className="date">
-            <i class="far fa-calendar-check"></i> November 2017
-          </p>
-          <p className="desc">
-            IEEE EMBS chapter INSAT was honored to provide a Community
-            Management training session with Mrs Amal Monsri
-          </p>
-          <img src={community_management_training} alt="pic" />
-          <p className="date">
-            <i class="far fa-calendar-check"></i>November, 2017
-          </p>
-          <p className="desc">
-            IEEE EMBS chapter INSAT, in collaboration with Mrs Aziza Inoubli and
-            Mr Ali Bouslimi, offered to students a professional Public Speaking
-            training Session.
-          </p>
-          <img src={public_speaking_training} alt="pic" />
-        </Col>
-      </Row>
-    </Container>
+          <Col className="box" md={{ span: 5 }} sm={{ span: 12 }}>
+            <p className="training-icon">
+              <i className="fas fa-briefcase"></i>
+            </p>
+            <p className="training-title">Technical Skills Trainings</p>
+            <p className="training-date">
+              <i className="far fa-calendar-check"></i> February 2018
+            </p>
+            <p className="training-desc">
+              In collaboration with the IMDTEC Tunisie, IEEE EMBS INSAT SB has
+              put at the disposal of some of its members a certified ISO 17025
+              training.
+            </p>
+            <img
+              className="training-img"
+              src={community_management_training}
+              alt="pic"
+            />
+            <p className="training-date">
+              <i className="far fa-calendar-check"></i> November, 2018
+            </p>
+            <p className="training-desc">
+              IEEE EMBS chapter INSAT gave an introduction to Bio-informatics
+              through a workshop combining biology, computer science and
+              mathematics conducted by Mr Ali Chamkhi
+            </p>
+            <img
+              className="training-img"
+              src={public_speaking_training}
+              alt="pic"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
