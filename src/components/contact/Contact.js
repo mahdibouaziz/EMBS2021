3f87f74d9318558c2e87491911e7483ae8f06c03
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2020-08-13 19:45:55 +0100

    gadit l contact page wmazelet l map

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -50,7 +50,25 @@ const Contact = () => {
               </Button>
             </form>
           </Col>
-          <Col md={4}></Col>
+          <Col md={4}>
+            <h3>
+              <i className="fas fa-location-arrow"></i> Location
+            </h3>
+            <p>INSAT Centre Urbain Nord BP 676 - 1080 Tunis Cedex</p>
+            <h3>
+              <i className="far fa-envelope"></i> E-mail
+            </h3>
+            <p>embs-insat@outlook.com</p>
+            <h3>
+              <i className="fas fa-phone-square-alt"></i> Phone
+            </h3>
+            <p>(+216) 58 869 350</p>
+            <p className="social">
+              <i className="fab fa-facebook"></i>
+              <i className="fab fa-instagram"></i>
+              <i className="fab fa-linkedin-in"></i>
+            </p>
+          </Col>
         </Row>
       </Container>
     </div>
