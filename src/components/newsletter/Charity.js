13e529f8f4c75d18466c6e3bc717232db78375f5
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2020-08-17 21:28:08 +0100

    gadit l charity fel newsletterpage

--- src/components/newsletter/Charity.js
+++ src/components/newsletter/Charity.js
@@ -1,7 +1,74 @@
 import React from "react";
+import { Container, Row } from "react-bootstrap";
+import "./styleNews.css";
+import pic2016 from "./img/charity/charity_2016.jpg";
+import pic2018 from "./img/charity/charity_2018.JPG";
 
 const Charity = () => {
-  return <section>charity</section>;
+  return (
+    <section>
+      <Container className="section">
+        <Row>
+          <h1 className="title">
+            <span>Charity</span> Work
+          </h1>
+        </Row>
+        <Row className="char-box-row">
+          <div className="char-box box1">
+            <img src={pic2016} alt="" />
+            <h2>Charity Day 2016</h2>
+            <p>
+              The event took place on February 14th which in fact coincides with
+              Valentine’s Day,
+            </p>
+            <p>
+              the international day of sharing and making people feel important
+              and special.
+            </p>
+            <p>
+              That day was dedicated to the unfortunate children at Charles
+              Nicolle’s hospital
+            </p>
+            <p>
+              a variety of workshops such as; the DIY (do-it-yourself) workshop.
+            </p>
+            <p>
+              Plenty of handmade crafts were created such as Snow white and
+              Superman’s cardboards
+            </p>
+            <p>There was also a photography workshop!</p>
+          </div>
+          <div className="char-box box2">
+            <h2>And More is yet</h2>
+            <h3>TO COME SOON</h3>
+            <p>The new executive board of</p>
+            <p>
+              IEEE INSAT Engineering in Medicine and Biology Society Chapter
+              team
+            </p>
+            <p>
+              is preparing a very varied and interesting program for this year.
+            </p>
+            <p>It includes multiple charity activities and more.</p>
+            <p>So stay tuned ! EMBS is preparing something big!</p>
+          </div>
+          <div className="char-box box3">
+            <img src={pic2018} alt="" />
+            <h2>Charity Day 2019</h2>
+            <p>We went to “Bechir Hamza Children’s Hospital“</p>
+            <p>offered them gifts and spent the day having fun</p>
+            <p>
+              and putting a smile on their faces by doing a bunch of activities
+              such as plays
+            </p>
+            <p>in collaboration with our friends from “Theatro INSAT“.</p>
+            <p>Donation we collected where from an “Eat to Donate“ stand</p>
+            <p>we held during our event ‘Health Tech’.</p>
+          </div>
+        </Row>
+      </Container>
+    </section>
+  );
 };
 
 export default Charity;
