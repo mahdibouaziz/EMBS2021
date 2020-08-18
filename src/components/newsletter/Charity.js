70ee84c5ad68b6cdca156414daa394118e342cde
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2020-08-18 15:13:14 +0100

    kamalt l newsletter page fini kima l site la9dim

--- src/components/newsletter/Charity.js
+++ src/components/newsletter/Charity.js
@@ -6,64 +6,76 @@ import pic2018 from "./img/charity/charity_2018.JPG";
 
 const Charity = () => {
   return (
-    <section>
-      <Container className="section">
+    <section className="charity-section">
+      <Container>
         <Row>
-          <h1 className="title">
-            <span>Charity</span> Work
-          </h1>
+          <h1 className="title">Charity Work</h1>
+          <p className="body">
+            Every year EMBS_INSAT organizes a charity day.
+            <br />A day where we go visit a children’s hospital. We spend the
+            day with them doing a bunch of activities, having fun and putting a
+            smile on their faces.
+          </p>
         </Row>
-        <Row className="char-box-row">
+        <Row className="main-row">
           <div className="char-box box1">
             <img src={pic2016} alt="" />
-            <h2>Charity Day 2016</h2>
-            <p>
-              The event took place on February 14th which in fact coincides with
-              Valentine’s Day,
-            </p>
-            <p>
-              the international day of sharing and making people feel important
-              and special.
-            </p>
-            <p>
-              That day was dedicated to the unfortunate children at Charles
-              Nicolle’s hospital
-            </p>
-            <p>
-              a variety of workshops such as; the DIY (do-it-yourself) workshop.
-            </p>
-            <p>
-              Plenty of handmade crafts were created such as Snow white and
-              Superman’s cardboards
-            </p>
-            <p>There was also a photography workshop!</p>
+            <div>
+              <h2>Charity Day 2016</h2>
+              <p>
+                The event took place on February 14th which in fact coincides
+                with Valentine’s Day,
+              </p>
+              <p>
+                the international day of sharing and making people feel
+                important and special.
+              </p>
+              <p>
+                That day was dedicated to the unfortunate children at Charles
+                Nicolle’s hospital
+              </p>
+              <p>
+                a variety of workshops such as; the DIY (do-it-yourself)
+                workshop.
+              </p>
+              <p>
+                Plenty of handmade crafts were created such as Snow white and
+                Superman’s cardboards
+              </p>
+              <p>There was also a photography workshop!</p>
+            </div>
           </div>
           <div className="char-box box2">
-            <h2>And More is yet</h2>
-            <h3>TO COME SOON</h3>
-            <p>The new executive board of</p>
-            <p>
-              IEEE INSAT Engineering in Medicine and Biology Society Chapter
-              team
-            </p>
-            <p>
-              is preparing a very varied and interesting program for this year.
-            </p>
-            <p>It includes multiple charity activities and more.</p>
-            <p>So stay tuned ! EMBS is preparing something big!</p>
+            <div>
+              <h2>And More is yet</h2>
+              <h3>TO COME SOON</h3>
+              <p>The new executive board of</p>
+              <p>
+                IEEE INSAT Engineering in Medicine and Biology Society Chapter
+                team
+              </p>
+              <p>
+                is preparing a very varied and interesting program for this
+                year.
+              </p>
+              <p>It includes multiple charity activities and more.</p>
+              <p>So stay tuned ! EMBS is preparing something big!</p>
+            </div>
           </div>
           <div className="char-box box3">
-            <img src={pic2018} alt="" />
-            <h2>Charity Day 2019</h2>
-            <p>We went to “Bechir Hamza Children’s Hospital“</p>
-            <p>offered them gifts and spent the day having fun</p>
-            <p>
-              and putting a smile on their faces by doing a bunch of activities
-              such as plays
-            </p>
-            <p>in collaboration with our friends from “Theatro INSAT“.</p>
-            <p>Donation we collected where from an “Eat to Donate“ stand</p>
-            <p>we held during our event ‘Health Tech’.</p>
+            <div>
+              <img src={pic2018} alt="" />
+              <h2>Charity Day 2019</h2>
+              <p>We went to “Bechir Hamza Children’s Hospital“</p>
+              <p>offered them gifts and spent the day having fun</p>
+              <p>
+                and putting a smile on their faces by doing a bunch of
+                activities such as plays
+              </p>
+              <p>in collaboration with our friends from “Theatro INSAT“.</p>
+              <p>Donation we collected where from an “Eat to Donate“ stand</p>
+              <p>we held during our event ‘Health Tech’.</p>
+            </div>
           </div>
         </Row>
       </Container>
--- src/components/newsletter/Collaboration.js
+++ src/components/newsletter/Collaboration.js
@@ -7,29 +7,93 @@ import img4 from "./img/collabs/img4.jpg";
 
 const Collaboration = () => {
   return (
-    <section>
-      <Container className="section">
+    <section className="collab-section">
+      <Container>
         <Row>
-          <h1 className="title">
-            <span>Events</span> We Participed In
-          </h1>
+          <h1 className="title">Events We Participed In</h1>
         </Row>
         <Row>
+          <p className="body">
+            Our chapter takes part in yearly events, such as IEEE day, TSYP.. by
+            organising and attending them along with other INSAT student branch
+            chapters.
+          </p>
+        </Row>
+        <Row className="main-row">
           <div className="collab-box">
-            <img src={img1} alt="" />
-            <h2>IEEE Day</h2>
+            <a
+              href="https://www.facebook.com/pg/IeeeInsatStudentBranch/photos/?tab=album&album_id=1980250082013551&ref=page_internal"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              <img src={img1} alt="" />
+            </a>
+
+            <div className="main-row-text">
+              <h2>IEEE Day</h2>
+              <p className="views">
+                <i className="far fa-eye"></i> 1k Views
+                <br />
+                <i className="far fa-comments"></i> 07{"  "}
+                <i className="fas fa-share"></i> 14
+              </p>
+            </div>
           </div>
           <div className="collab-box">
-            <img src={img2} alt="" />
-            <h2>TSYP</h2>
+            <a
+              href="https://www.facebook.com/pg/IeeeInsatStudentBranch/photos/?tab=album&album_id=2085121378193087&ref=page_internal"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              <img src={img2} alt="" />
+            </a>
+
+            <div className="main-row-text">
+              <h2>TSYP</h2>
+              <p className="views">
+                <i className="far fa-eye"></i> 2k Views
+                <br />
+                <i className="far fa-comments"></i> 14{"  "}
+                <i className="fas fa-share"></i> 22
+              </p>
+            </div>
           </div>
           <div className="collab-box">
-            <img src={img3} alt="" />
-            <h2>EMBS Germany</h2>
+            <a
+              href="https://www.facebook.com/pg/EMBS.INSAT/photos/?tab=album&album_id=313871312627473&ref=page_internal"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={img3} alt="" />
+            </a>
+
+            <div className="main-row-text">
+              <h2>EMBS Germany</h2>
+              <p className="views">
+                <i className="far fa-eye"></i> 209 Views
+                <br />
+                <i className="far fa-comments"></i> 02{"  "}
+                <i className="fas fa-share"></i> 06
+              </p>
+            </div>
           </div>
           <div className="collab-box">
-            <img src={img4} alt="" />
-            <h2>Summer School</h2>
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+              {" "}
+              <img src={img4} alt="" />
+            </a>
+
+            <div className="main-row-text">
+              <h2>Summer School</h2>
+              <p className="views">
+                <i className="far fa-eye"></i> 1.8k Views
+                <br />
+                <i className="far fa-comments"></i> 14{"  "}
+                <i className="fas fa-share"></i> 22
+              </p>
+            </div>
           </div>
         </Row>
       </Container>
